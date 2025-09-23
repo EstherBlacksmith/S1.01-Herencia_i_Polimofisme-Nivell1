e491d557e3d08f5b89a1f3c567f581559abfd118
ehguzman <ehguzman@gmail.com>
2025-09-23 12:52:10 +0200

    Deleting the incorrect word "Instrument" from README file and classes

--- src/main/java/instruments/Percussion.java
+++ src/main/java/instruments/Percussion.java
@@ -2,7 +2,7 @@ package instruments;
 
 public class Percussion extends Instruments {
     static {
-        System.out.println("Initializing instruments.Percussion class");
+        System.out.println("Initializing Percussion class");
     }
     public static String message ="It's playing a percussion instrument";
 
